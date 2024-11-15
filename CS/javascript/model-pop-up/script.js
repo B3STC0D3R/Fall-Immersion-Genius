6b2e4c7b9976e51c02364252853e77eba47032fd
B3STC0D3R <naim.mccraw@hgs.hiddengeniusproject.org>
2024-11-15 17:49:17 -0500

    Update script.js

--- CS/javascript/model-pop-up/script.js
+++ CS/javascript/model-pop-up/script.js
@@ -1,3 +1,20 @@
-function modelPopUp() {
-    // Your code here.
-}   
+
+    document.addEventListener("DOMContentLoaded", () => {
+
+        const modal = document.getElementById("myModal");
+        const openModal = document.getElementById("openModal");
+        const closeModal = document.getElementById("closeModal");
+
+        openModal.addEventListener("click", () => {
+            modal.style.display = "none";
+        });
+        closeModal.addEventListener("click", () => {
+            modal.style.display = "none";
+        });
+        window.addEventListener("click", (event) => {
+            if (event.target == modal){
+                modal.style.display = "none";        
+            }
+            
+        });
+    });
