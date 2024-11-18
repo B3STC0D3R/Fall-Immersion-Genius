20ec248110181ee2859b110a125e058cc1e4d307
B3STC0D3R <naim.mccraw@hgs.hiddengeniusproject.org>
2024-11-18 17:09:54 -0500

    Update script.js 2.0

--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -1,3 +1,5 @@
-function eventHandling() {
-    // Your code here.
-}
+document.getElementById('myButton').addEventListener('click',function(){
+    image = document.createElement('img');
+    image.src = "https://teeturtle.com/cdn/shop/files/Elemental-Kitsune_800x800_SEPS.jpg?v=1703408503";
+    document.body.appendChild(image);
+});
